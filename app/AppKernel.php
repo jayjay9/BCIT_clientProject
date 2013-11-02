90059d6c186b0f54919f967059ea534504b423ae
Alfredo Rodriguez <alfredo.rdz.joya@gmail.com>
2013-11-01 23:50:28 -0700

    This commit removes the line regarding FOS Bundle from AppKernel.php

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Bc\Bundle\BootstrapBundle\BcBootstrapBundle(),
             new Earls\LeaseBundle\EarlsLeaseBundle(),
-			new FOS\UserBundle\FOSUserBundle(),
+			
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {

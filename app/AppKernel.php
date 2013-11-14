469c3d313a4bd3fb915338e5ed1e6bf9bcc54966
BlueMinnow <morrischanfree@hotmail.com>
2013-11-14 13:44:00 -0800

    Completed Login

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Bc\Bundle\BootstrapBundle\BcBootstrapBundle(),
             new Earls\LeaseBundle\EarlsLeaseBundle(),
+            
 			new FOS\UserBundle\FOSUserBundle(),
             new Earls\UserBundle\UserBundle(),
         );
